3a9a40006a5b5fe5e8a5bd3b0e0a9d2aa3098302
SidharthR27 <theroyalguy7@gmail.com>
2025-01-08 10:50:01 +0530

    updated

--- mailapp/static/mailapp/script.js
+++ mailapp/static/mailapp/script.js
@@ -2,4 +2,4 @@ number_input = document.getElementById("id_mobile_number");
 number_input.value= "+91";
 
 message_input = document.getElementById("id_message");
-message_input.value= "Hi Future Me...";
+message_input.value= "Hi Future Me ...";
